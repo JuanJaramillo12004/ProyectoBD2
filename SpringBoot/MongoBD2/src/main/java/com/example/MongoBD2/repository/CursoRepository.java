fdcbfaa15cb00b5ef77ba4db3cca10cf707ee227
JaramilloTR12 <juan.e.jara12004@gmail.com>
2024-05-03 21:14:58 -0500

    up

--- SpringBoot/MongoBD2/src/main/java/com/example/MongoBD2/repository/CursoRepository.java
+++ SpringBoot/MongoBD2/src/main/java/com/example/MongoBD2/repository/CursoRepository.java
@@ -9,6 +9,9 @@ import java.util.List;
 
 @Repository
 public interface CursoRepository extends MongoRepository<CursoModel, Integer>{
-
+    @Aggregation({
+            "{'$lookup': { 'from': 'Calificaciones', 'localField': '_id', 'foreignField': 'id_curso', 'as': 'Calificaciones' } }",
+            "{'$unwind': '$Calificaciones' }",
+            "{'$match': { 'Calificaciones.calificacion': { '$gte': ?0 } } }"})
     List<CursoModel> listarCursosCalificacionesMayoresAN(Double calificacion);
 }
